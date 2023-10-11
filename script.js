c0e6953687deb3bf72c70f88f2a171cbb0ff5271
Rodrigues Kévin <123478789+Rodrigues-Kevin@users.noreply.github.com>
2023-10-11 11:31:09 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function responsiveNav() {
-    var x = document.getElementById("nav");
-    if (x.className === "nav") {
-      x.className += " responsive";
-    }
-    else {
-      x.className = "nav";
-    }
-}
